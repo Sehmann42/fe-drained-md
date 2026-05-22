0272de4b9f75a74a63fb72127f1ff3f178a94b78
Sehmann42 <stefan.ehmann2@gmail.com>
2026-05-22 19:30:00 +0200

    Card Rarity for collection

--- src/assets/components/page_blocks/cards/YGOCard.jsx
+++ src/assets/components/page_blocks/cards/YGOCard.jsx
@@ -1,10 +1,28 @@
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import "../../../css/Collection/ygocard.css"
 
 const YGOCard = ({children, cardData}) => {
+
+    const [cardRarity, setCardRarity] = useState("")
+
+    useEffect(() => {
+        
+        const rarity = cardData.rarity
+
+        console.log(rarity)
+
+        if (rarity == "SR"){
+            setCardRarity("isSR")
+        }
+        if (rarity == "UR"){
+            setCardRarity("isUR")
+        }
+
+    }, []);
+
     return <>
-        <div className=" ygocard d-flex justify-content-center">
+        <div className={" ygocard d-flex justify-content-center " + cardRarity}>
             <img loading="lazy"
                 src={cardData.image_url}
             ></img>
