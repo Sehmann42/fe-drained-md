d6dcf7d686a7aab3bd31e08612b0e10d16c79b40
Sehmann42 <stefan.ehmann2@gmail.com>
2026-05-26 18:33:58 +0200

    Icon and Credits Fix

--- src/assets/components/pages/credits/CreditsPage.jsx
+++ src/assets/components/pages/credits/CreditsPage.jsx
@@ -3,11 +3,11 @@ import PageHeader from "../../page_blocks/header/Header"
 
 const CreditsPage = () => {
     return <>
-    <div className="h-100 main-background">
+    <div className="d-flex flex-column main-background h-100">
         <PageHeader />
 
         <div className=" h-100 d-flex justify-content-center text-center">
-            <span>Icons From : https://www.flaticon.com/</span>
+            <span>Icons From : <a>https://www.flaticon.com/</a></span>
         </div>
 
         <PageFooter />
