8c018e68fc964b066a50eddc9cf69a3ceabb5fd8
Sehmann42 <stefan.ehmann2@gmail.com>
2026-06-18 22:47:00 +0200

    Updates

---
+++ assets/Collection-DvEam8y9.js
@@ -0,0 +1,7 @@
+import{g as e,p as t}from"./index-B7_HIETF.js";var n=e(),r=t(),i=e=>{let t=(0,n.c)(11),{children:i,elementsPerRow:a,maxHeight:o}=e,s=o===void 0?`95%`:o,c;t[0]===Symbol.for(`react.memo_cache_sentinel`)?(c={minHeight:0,overflow:`hidden`},t[0]=c):c=t[0];let l;t[1]===s?l=t[2]:(l={height:s},t[1]=s,t[2]=l);let u=`repeat(`+a+`, 1fr)`,d;t[3]===u?d=t[4]:(d={gridAutoRows:`auto`,gridTemplateColumns:u},t[3]=u,t[4]=d);let f;t[5]!==i||t[6]!==d?(f=(0,r.jsx)(`div`,{style:d,className:` 
+                        collectionContent
+                        overflow-auto
+                        h-100 
+                        w-100
+                        p-2
+                        `,children:i}),t[5]=i,t[6]=d,t[7]=f):f=t[7];let p;return t[8]!==l||t[9]!==f?(p=(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(`div`,{style:c,className:` flex-grow-1`,children:(0,r.jsx)(`div`,{className:` h-100 p-3`,children:(0,r.jsx)(`div`,{style:l,className:` collection`,children:f})})})}),t[8]=l,t[9]=f,t[10]=p):p=t[10],p};export{i as t};
