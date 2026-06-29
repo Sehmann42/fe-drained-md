5bba29097c07a3b8386d18579a8c3ed75ed16c86
Sehmann42 <stefan.ehmann2@gmail.com>
2026-06-29 13:03:50 +0200

    Campaign item Rework Done

--- src/assets/components/page_blocks/campaign/CampaignItem.jsx
+++ src/assets/components/page_blocks/campaign/CampaignItem.jsx
@@ -11,15 +11,11 @@ const CampaignItem = ({data, handleOnClickEvent, openCampaignEditModal}) => {
     }
 
     return <>
-    <div style={{ height: "200px" }} className=" position-relative" >
-        <div className=" h-100 campaignItem d-flex flex-column p-3" onClick={() => handleOnClickEvent(CampaignItemData.campaignId)}>
-            <h5 className=" text-center">
-                {CampaignItemData.campaignName}
-            </h5>
-            <div>
-                <br />
-            </div>
-            <div className=" overflow-auto d-flex flex-column" style={{maxHeight: "150px"}}>
+    <div style={{ height: "200px" }} className="card position-relative function-background" style={{width: "17rem;"}}>
+        <img style={{ height: "150px" }} className="card-img-top" src={import.meta.env.VITE_BASE + "/icons/other/campaign_default_pic.png"} alt="Card image cap" /> 
+        <div className="card-body ">
+            <h5 style={{color: "white"}} className="">{CampaignItemData.campaignName}</h5>
+            <div className=" overflow-auto d-flex flex-column hidden-bar" style={{maxHeight: "100px"}}>
                 <b>Spieler:</b> {
                 
                     CampaignItemData.campaignPlayers.map((data) => {
@@ -27,12 +23,11 @@ const CampaignItem = ({data, handleOnClickEvent, openCampaignEditModal}) => {
                     })
                 
                 }
-            </div> 
+            </div>
+            <a onClick={() => handleOnClickEvent(CampaignItemData.campaignId)} className="btn btn-primary">Your Move!</a>
+
+            <EditCampaignItem editCampaignModalData={CampaignItemData} openCampaignEditModal={openCampaignEditModal}/>
         </div>
-
-        <EditCampaignItem editCampaignModalData={CampaignItemData} openCampaignEditModal={openCampaignEditModal}/>
-
-        
     </div>
 
     </>
