fcc5ae50b3eff4c9dd8eaf8d2da2653cf50b83b2
Sehmann42 <stefan.ehmann2@gmail.com>
2026-07-03 13:03:20 +0200

    Build Folder

---
+++ dist/assets/Collection-gHeZDQm1.js
@@ -0,0 +1,7 @@
+import{g as e,p as t}from"./index-B-TDALQo.js";var n=e(),r=t(),i=e=>{let t=(0,n.c)(12),{children:i,elementsPerRow:a,maxHeight:o,gap:s}=e,c=o===void 0?`95%`:o,l=s===void 0?`1rem`:s,u;t[0]===Symbol.for(`react.memo_cache_sentinel`)?(u={minHeight:0,overflow:`hidden`},t[0]=u):u=t[0];let d;t[1]===c?d=t[2]:(d={height:c},t[1]=c,t[2]=d);let f=`repeat(`+a+`, 1fr)`,p;t[3]!==l||t[4]!==f?(p={gridAutoRows:`auto`,gridTemplateColumns:f,gap:l},t[3]=l,t[4]=f,t[5]=p):p=t[5];let m;t[6]!==i||t[7]!==p?(m=(0,r.jsx)(`div`,{style:p,className:` 
+                        collectionContent
+                        overflow-auto
+                        h-100 
+                        w-100
+                        p-2
+                        `,children:i}),t[6]=i,t[7]=p,t[8]=m):m=t[8];let h;return t[9]!==d||t[10]!==m?(h=(0,r.jsx)(r.Fragment,{children:(0,r.jsx)(`div`,{style:u,className:` flex-grow-1`,children:(0,r.jsx)(`div`,{className:` h-100 p-3`,children:(0,r.jsx)(`div`,{style:d,className:` collection`,children:m})})})}),t[9]=d,t[10]=m,t[11]=h):h=t[11],h};export{i as t};
